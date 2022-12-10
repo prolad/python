57f6c33492aaa870e5eff3ffed210664a0917cea
prolad <86721842+prolad@users.noreply.github.com>
2022-12-10 16:42:39 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+op = input("enter desired operation[ +,-,*,/: ")
+
+if op == '+':
+    print(num1 + num2)
+
+elif op == '-':
+    print(num1 - num2)
+
+elif op == '*':
+    print(num1 * num2)
+
+elif op == '/':
+    print(num1 / num2)
